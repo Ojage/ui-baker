2e133b48a397047c93e5d5ab67f9b33a09b7fc22
OJAGE S <45638270+Ojage@users.noreply.github.com>
2022-09-03 15:20:53 +0100

    Conecting to github, ran create-react-app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { db } from './firebase-config';
+import { collection, getDocs } from 'firebase/firestore';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+// const [Blog, setBlogs] = useState([]);
+// const BlogPostsCollectionRef = collection(db, "Blog")
+// useEffect(() => {
+//   const getBlogPosts = async () => {
+//     const data = await getDocs()
+//     console.log(data);
+// }
+//   getBlogPosts()
+// }, [])
   return (
     <div className="App">
       <header className="App-header">
---
+++ src/firebase-config.js
@@ -0,0 +1,17 @@
+// import { initializeApp } from "firebase/app";
+// import { getFirestore } from "@firebase/firestore";
+
+// const firebaseConfig = {
+//     apiKey: "AIzaSyB2JxR2pe3LW3ZyrWW5YK6jRwmVoWyq-s8",
+//     authDomain: "ui-baker.firebaseapp.com",
+//     projectId: "ui-baker",
+//     storageBucket: "ui-baker.appspot.com",
+//     messagingSenderId: "373242371279",
+//     appId: "1:373242371279:web:0617aafb72c6aa6540ebba",
+//     measurementId: "G-QWYPKW51BJ"
+//   };
+
+
+//   const app = initializeApp(firebaseConfig);
+
+//   export const db = getFirestore(app);
